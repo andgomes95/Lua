a6f003eb66c836eab126abc72db81817aee83462
Andre Gomes <andgomes95@gmail.com>
2018-06-27 00:58:30 -0300

    fix produto class

--- main.lua
+++ main.lua
@@ -6,32 +6,16 @@ require "itemvenda"
 -- Trabalho de CLP
 -- UFSJ
 -- Andre, Carlos, Joao
---lista os produtos
-function lista_produtos (produtos)
-    for pos,item in ipairs(produtos)
-        do
-            codigo,nome,valor=item:verProduto()
-            print("Cod:",codigo,"Nome:",nome,"Valor:",valor)
-        end
-end
--- lista os itens
-function lista_itens (itens)
-    for pos,item in ipairs(itens)
-        do
-            nome,valor,qtd=item:getItens()
-            print("Nome:",nome,"Valor:",valor,"Quantidade",qtd)
-        end
-end
 
 -- ver os clientes cadastrados
 function lista_clientes (clientes)
-    if(num_clientes<1)
+    if(#clientes<1)
         then
             print("Lista Vazia, Nenhum cliente foi cadastrado.")
             return nill
         end
-    print("Número de clientes cadastrados:",num_clientes)
-    for i=0,num_clientes-1,1
+    print("Número de clientes cadastrados:",#clientes)
+    for i=1,#clientes,1
         do
             --pega o retorno múltiplo de dados
             nome,endereco,rg,data=clientes[i]:verCliente()
@@ -39,20 +23,9 @@ function lista_clientes (clientes)
         end
 end
 
--- ver o carrinho_final
-
-function VerCarrinho(carrinho_final)
-    print("---Resumo Compra --")
-    for pos,item in ipairs(carrinho_final)
-        do
-            codigo,nome,valor=item:verProduto()
-            print("Cod:",codigo,"Nome:",nome,"Valor:",valor)
-        end
-end
-
-function adicionarCliente(clientes,num_clientes)
+function adicionarCliente(clientes)
     local cliente=Cliente:new(nill, nill)
-    clientes[num_clientes]=cliente
+    clientes[#clientes+1]=cliente
     print("Cadastrando Cliente")
     print("Nome:")
     local nome=io.read()
@@ -64,8 +37,7 @@ function adicionarCliente(clientes,num_clientes)
     local nascimento= io.read()
     -- Cadastra um cliente
     cliente:alteraCliente(nome,endereco,rg,nascimento)
-    num_clientes=num_clientes+1
-    return clientes,num_clientes
+    return clientes
 end
 
 function alterarCliente(clientes)
@@ -90,17 +62,86 @@ function alterarCliente(clientes)
     return clientes
 end
 
-function removeCliente(clientes,num_clientes)
+function removeCliente(clientes)
     print("Digite o nome do cliente")
-    cliente=io.read()
+    local cliente=io.read()
     for i=0,#clientes do
         if clientes[i]:getNome() == cliente then
-            clientes[i]:removeCliente()
+            clientes[i]:destroiCliente()
             table.remove(clientes,i)
-            num_clientes = num_clientes-1
         end
     end
     return clientes,num_clientes
+end
+
+--lista os produtos
+function lista_produtos (produtos)
+    for pos,item in ipairs(produtos)
+        do
+            codigo,nome,valor=item:verProduto()
+            print("Cod:",codigo,"Nome:",nome,"Valor:",valor)
+        end
+end
+
+function adicionarProduto(produtos)
+    local indice =  #produtos+1
+    local produto = Produto:new(nill,nill)
+    produtos[indice] = produto
+    print("Digite o nome do produto")
+    local nome=io.read()
+    print("Digite o valor do produto")
+    local valor = io.read()
+    produto:alterarProduto(001+indice,nome,valor)
+    return produtos
+end
+
+function alterarProduto(produtos)
+    print("Digite o nome do produto")
+    local produto=io.read()
+     for i=0,#produtos do
+        if produtos[i]:getNome() == produto then
+            print("Nome:")
+            local nome=io.read()
+            print("Valor:")
+            local valor = io.read()
+            -- Cadastra um cliente
+            produtos[i]:alterarProduto(produtos[i]:getCodigo(),nome,valor)
+            return produtos
+        end
+    end
+    print ("Produto não existente")
+    return produtos 
+end
+
+function removeProduto(produtos)
+    print("Digite o nome do cliente")
+    local produto=io.read()
+    for i=0,#produtos do
+        if produtos[i]:getNome() == produto then
+            produtos[i]:destroiProduto()
+            table.remove(produtos,i)
+        end
+    end
+    return produtos
+end
+
+-- lista os itens
+function lista_itens (itens)
+    for pos,item in ipairs(itens)
+        do
+            nome,valor,qtd=item:getItens()
+            print("Nome:",nome,"Valor:",valor,"Quantidade",qtd)
+        end
+end
+
+-- ver o carrinho_final
+function VerCarrinho(carrinho_final)
+    print("---Resumo Compra --")
+    for pos,item in ipairs(carrinho_final)
+        do
+            codigo,nome,valor=item:verProduto()
+            print("Cod:",codigo,"Nome:",nome,"Valor:",valor)
+        end
 end
 
 
@@ -108,8 +149,7 @@ end
 clientes={}
 --lista dos produtos
 produtos={}
---numero de clientes cadastrados
-num_clientes=0
+
 qtd_itens=1
 num_venda=0
 
@@ -120,7 +160,7 @@ for i=0,5,1
     do
         produto=Produto:new(nill,nill)
         produtos[i]=produto
-        produto:cadastraProduto(i+001,"Produto "..(i),25.00+i)
+        produto:alterarProduto(i+001,"Produto "..(i),25.00+i)
     end
 
 flag=1
@@ -128,40 +168,45 @@ while(flag)
 do
     -- Menu de interacação
     print("Escolha uma Opção")
-    print("0- Para encerrar\n1- Cadastrar Cliente\n2- Ver Clientes Cadastrados\n3- Alterar Cliente\n4- Remover Clientes\n5- Listar Produtos Disponíveis\n6- Iniciar Venda\n")
+    print("0- Para encerrar\n1- Cadastrar Cliente\n2- Ver Clientes Cadastrados\n3- Alterar Cliente\n4- Remover Clientes\n5- Adicionar Produto\n6- Listar Produtos Disponíveis\n7- Alterar Produtos\n8- Remove Produto\n9- Iniciar Venda\n")
     opcao=io.read()
     print("opcao",opcao)
 
     -- cadastrar clientes
     if opcao == "1" then
         clientes,num_clientes = adicionarCliente(clientes,num_clientes)
-    end
     -- listar clientes
-    if opcao == "2" then
+    elseif opcao == "2" then
         print("--Clientes--")
         print(lista_clientes(clientes))
         print("-------------")
-    end
     -- alterar clientes
-    if opcao == "3" then
+    elseif opcao == "3" then
         clientes =  alterarCliente(clientes)
-    end
     -- Remove Cliente
-    if opcao == "4" then
+    elseif opcao == "4" then
         clientes,num_clientes =  removeCliente(clientes,num_clientes)
-    end
-    -- listar produtos
-    if opcao == "5" then
-        print(lista_produtos(produtos))
-    end
 
+    -- listar produtos
+    elseif opcao == "5" then
+        produtos = adicionarProduto(produtos)
+
+    elseif opcao == "6" then
+        lista_produtos(produtos)
+
+    elseif opcao == "7" then
+        produtos = alterarProduto(produtos)
+    elseif opcao == "8" then
+        produtos = removeProduto(produtos)
     -- Inicia uma nova venda
-    if opcao == "6" then
+    elseif opcao == "9" then
         print("--Nova Compra---")
         -- cria a instancia de uma nova venda
         novacompra=Venda:new(nill,nill)
 
         carrinho_final={}
+
+        
 
         print("Adicione Produtos no Carrinho")
         --carrinho de produtos --
@@ -231,12 +276,13 @@ do
 
             end
         end
-    end
 
 
 
-    if opcao == "0" then
+    elseif opcao == "0" then
         print("Encerrado")
         break
+    else
+        print ("Opção Invalida")
     end
 end
